19d052f85e64e365a9466de06a875dd6be48ff66
Louis Wong <louiswong@Louiss-MacBook-Pro.local>
2018-12-24 22:12:19 -0500

    Created movie/customer/genre/rental models and movies/customers/genres/modelsroutes

---
+++ models/movie.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+const { genreSchema } = require('./genre');
+
+const movieSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 5,
+    maxlength: 50
+  },
+  genre: { 
+    type: genreSchema,  
+    required: true
+  },
+  numberInStock: {
+    type: Number,
+    require: true,
+    min: 0,
+    max: 255
+  },
+  dailyRentalRate: {
+    type: Number,
+    require: true,
+    min: 0,
+    max: 255
+  }
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+function validateMovie(movie) {
+  const schema = {
+    title: Joi.string().min(5).max(50).required(),
+    genreId: Joi.string().required(),
+    numberInStock: Joi.number().min(0).required(),
+    dailyRentalRate: Joi.number().min(0).required()
+  }
+  return Joi.validate(movie, schema);
+}
+
+module.exports.Movie = Movie;
+module.exports.validate = validateMovie;
